ac4934cd646c054ec8ef74e538725b0d89c7649e
Grauero <grayero6996@gmail.com>
2019-04-04 16:23:59 +0300

    Initial setup for web app

---
+++ public/src/js/app.js
@@ -0,0 +1,26 @@
+const button = document.querySelector('button');
+
+function animate() {
+  const title = document.querySelector('.title');
+  const courseFeatureElements = document.querySelectorAll('.course-feature');
+
+  title.classList.remove('animate-in');
+  for (let element of courseFeatureElements) {
+    element.classList.remove('animate-in');
+  }
+  button.classList.remove('animate-in');
+
+  setTimeout(() => title.classList.add('animate-in'), 1000);
+  setTimeout(() => courseFeatureElements[0].classList.add('animate-in'), 3000);
+  setTimeout(() => courseFeatureElements[1].classList.add('animate-in'), 4500);
+  setTimeout(() => courseFeatureElements[2].classList.add('animate-in'), 6000);
+  setTimeout(() => courseFeatureElements[3].classList.add('animate-in'), 7500);
+  setTimeout(() => courseFeatureElements[4].classList.add('animate-in'), 9000);
+  setTimeout(() => courseFeatureElements[5].classList.add('animate-in'), 10500);
+  setTimeout(() => courseFeatureElements[6].classList.add('animate-in'), 12000);
+  setTimeout(() => button.classList.add('animate-in'), 13500);
+}
+
+animate();
+
+button.addEventListener('click', animate);
