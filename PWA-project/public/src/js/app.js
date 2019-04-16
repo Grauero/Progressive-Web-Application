d3d5fa9018bb8e4d9ed2ecccefae99c2a6c1b2cc
Grauero <grayero6996@gmail.com>
2019-04-16 15:59:23 +0300

    Add notification subscription from app.js

--- PWA-project/public/src/js/app.js
+++ PWA-project/public/src/js/app.js
@@ -2,6 +2,7 @@
 
 //variable for storing prompt event to call it in feed.js
 let deferredPrompt;
+const enableNotificationsbuttons = document.querySelectorAll('.enable-notifications');
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
@@ -15,3 +16,27 @@ window.addEventListener('beforeinstallprompt', e => {
   deferredPrompt = e;
   return false;
 });
+
+if ('Notification' in window) {
+  for (let i = 0; i < enableNotificationsbuttons.length; i++) {
+    enableNotificationsbuttons[i].style.display = 'inline-block';
+    enableNotificationsbuttons[i].addEventListener('click', askForNotificationPermission);
+  }
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission().then(result => {
+    console.log('User notification choice', result);
+    if (result !== 'granted') {
+      console.log('No permission granted');
+    } else {
+      displayConfirmNotification();
+    }
+  });
+}
+
+function displayConfirmNotification() {
+  new Notification('Successfully subscribed', {
+    body: 'You successfully subscribed to Notification service'
+  });
+}
